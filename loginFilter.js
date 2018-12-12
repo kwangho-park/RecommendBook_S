a32dafdd6b5761b5c7c031052f2f31602b88bab9
kwangho-park <44250982+kwangho-park@users.noreply.github.com>
2018-12-12 18:18:00 +0900

    login update
    모듈화 : closure 구조 적용하여 login filter기능의 모듈화

--- loginFilter.js
+++ loginFilter.js
@@ -1,7 +1,7 @@
 
 
 
-  
+
   var loginBtn;
   
   /* web browser에 웹 페이지가 로딩 시 동작하는 함수 */
@@ -11,23 +11,52 @@
 	  loginBtn.addEventListener("click",login);
   } // init() END
   
+  
+  /* login button click 시 실행되는 함수*/
   function login(){
-	    var id = document.getElementById("id");
-	    var pw = document.getElementById("pw");
+	  
+	    /* <input> tage의 dom객체의 참조값을 셋팅 */
+	    var idDom = document.getElementById("id");
+	    var pwDom = document.getElementById("pw");
 	    
 	    
-	    // id, pw 필터링 logic
-	    // value property : 입력된 값이 저장되어있는 변수
+	    // 사용자입력 id,pw
+	    var id = idDom.value;
+	    var pw = pwDom.value;
 	    
-	    if((id.value != "") && isNaN(id.value)){		// isNaN : is + Not a Number : 문자열일경우 true
-	    	if(pw.value!="" && !isNaN(pw.value)){
-	    		alert("로그인 성공!!");
-	    	}else{
-	    		alert("PW를 숫자로 입력하세요");
-	    	}
-	    }else{
-	    	alert("ID를 문자로 입력하세요!!");
-	    }
-
+	    
+	    // login 정보 필터링을 위한 객체생성 및 초기화
+	    var loginFilter = filter(id, pw);
+	    
+	    // filtering 결과 출력
+	    alert(loginFilter.filter());
 
   } // login() END
+  
+  
+  /* 로그인정보(id,pw)필터링 function */
+  // id, pw = global variable
+  function filter(id, pw){
+	  
+	  // filter을 위한 객체 정의
+	  return{
+		  filter : function(){
+			  
+			  var result = false;				// result = local variable
+
+			  if((id != "") && isNaN(id)){		// isNaN : is + Not a Number : 문자열일경우 true
+			  	if(pw !="" && !isNaN(pw)){
+			  		alert("로그인 성공!!");
+			  		result = true;
+			  	}else{
+			  		alert("PW를 숫자로 입력하세요");
+			  	}
+			    }else{
+			    	alert("ID를 문자로 입력하세요!!");
+			    }
+			  
+			  return result;
+		  }
+	  }
+	  
+  } // filter() END
